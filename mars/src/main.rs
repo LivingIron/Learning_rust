51cf984152296c3163e3e5a944111a9ce4772f4d
LivingIron <cveganton@gmail.com>
2021-02-18 17:43:49 +0200

    Learning the borrowing concept and some small practices

--- mars/src/main.rs
+++ mars/src/main.rs
@@ -1,10 +1,16 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
-    calculate_weight_on_mars(100.0);
-    
+    let mut input=String::new();
+
+    io::stdin().read_line(&mut input);
+    println!("Input : {}", input);
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars : {}kg",mars_weight);
 }
 
 
 fn calculate_weight_on_mars(weight: f32) -> f32{
-    50.0
+   (weight/9.81)*3.711
 }
