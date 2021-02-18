0f47b6f85c1bca46871dc69d42294d71254dad5d
LivingIron <cveganton@gmail.com>
2021-02-18 18:19:58 +0200

    Added a few comments

--- mars/src/main.rs
+++ mars/src/main.rs
@@ -1,9 +1,11 @@
+// getting the standart library and using the basic io
 use std::io;
 
 
 fn main() {
     let mut input=String::new();
 
+// You can either create 1 mutable reference or many non mutable ones
     io::stdin().read_line(&mut input);
     println!("Input : {}", input);
     let mars_weight = calculate_weight_on_mars(100.0);
